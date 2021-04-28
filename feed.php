53c776c7963007acc0514cc91a016cde845f957a
DaraKonoplova <83348659+DaraKonoplova@users.noreply.github.com>
2021-04-28 15:31:26 +0300

    Konkurs Avtoservis
    ------

---
+++ feed.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="/css/Style.css" rel="stylesheet"  type="text/css">
+    <title>AvtoServus_Help</title>
+</head>
+<body>
+<div id="Header"> 
+
+       
+       <div id="logo"> 
+       <a href="AvtoServis.php" id="Home"> <nk>Avto</nk>Servus   </a>
+       </div>
+
+       <div id="OoUs"> 
+       <a href="/about.php">
+        <div style="margin-right:5%;" id="Home2">О нас</div>
+      </a> 
+
+      <a href="/feed.php" id="Home22">
+     <div>Обратная связь</div>
+    </div>
+      </a>
+
+
+    <div id="Reg">
+      <a href="/reg.php" > Регистрация </a> | <a href="/avt.php"> Авторизация</a>
+    </div>
+       </div>
+
+
+
+
+    
+</body>
+</html>
